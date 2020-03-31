9431838fd13add0142417c8683fb912f608dc938
LeoPrivate <leo.menschenfreund@epita.fr>
2020-03-31 16:57:29 +0200

    terraform: change billing method for dynamodb table

--- terraform-poc/remote-storage/storage.tf
+++ terraform-poc/remote-storage/storage.tf
@@ -10,13 +10,20 @@ resource "aws_s3_bucket" "remote-state-storage" {
         prevent_destroy = true
     }
 
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+
 }
 
 resource "aws_dynamodb_table" "lock-for-s3" {
     name = "lock-terraform-remote-storage"
     hash_key = "LockID"
-    read_capacity = 20
-    write_capacity = 20
+    billing_mode = "PAY_PER_REQUEST"
 
     attribute {
         name = "LockID"
